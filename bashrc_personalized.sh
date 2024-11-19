461595fe3bc44335c35519f6b741f46cc34301b5
Guilherme Parreira <guilhermeparreira.silva@gmail.com>
2024-11-19 08:16:35 -0300

    bash personalized

---
+++ bashrc_personalized.sh
@@ -0,0 +1,9 @@
+parse_git_info() {
+    git rev-parse --is-inside-work-tree &>/dev/null || return
+    repo=$(basename "$(git rev-parse --show-toplevel 2>/dev/null)")
+    branch=$(git branch 2>/dev/null | grep '^*' | colrm 1 2)
+    [ -n "$(git status --porcelain 2>/dev/null)" ] && changes="*" || changes=""
+    echo " $repo $(tput setaf 3)$branch$(tput sgr0) $changes"
+}
+PS1="\[\e[32m\]\u\[\e[0m\]\[\e[34m\]\w\[\e[0m\]\$(parse_git_info)\n\$ "
+
